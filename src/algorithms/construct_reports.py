aa7be467b440e81322be1a8c451ed11c805a655f
Khakim <beksapin@mail.ru>
2021-04-22 13:41:01 +0600

    Cosmetic changes with prints in construct_reports.py

--- src/algorithms/construct_reports.py
+++ src/algorithms/construct_reports.py
@@ -391,3 +391,17 @@ def construct_report(conn, cursor):
 
     result = result.reset_index()
     commit_to_db(conn, cursor, "report", result)
+
+
+def delete_trash(cursor, conn):
+    dt = datetime.datetime.now()
+    cursor.execute(f"delete "
+                   f"from report "
+                   f"where update_datetime >= timestamp '{dt.year}-{dt.month}-{dt.day} 00:00:00' "
+                   f"and update_datetime < timestamp '{dt.year}-{dt.month}-{dt.day} 16:30:00';")
+    cursor.execute(f"delete "
+                   f"from dislocation "
+                   f"where update_datetime >= timestamp '{dt.year}-{dt.month}-{dt.day} 00:00:00' "
+                   f"and update_datetime < timestamp '{dt.year}-{dt.month}-{dt.day} 16:30:00';")
+
+    conn.commit()
