544375620cc36f2498b0c622b6e78c4a06f23db7
Khakim <beksapin@mail.ru>
2021-06-24 07:49:41 +0600

    manual uploading 2021/06/23 17:00

--- src/algorithms/load_dislocation/load_dislocation.py
+++ src/algorithms/load_dislocation/load_dislocation.py
@@ -104,5 +104,5 @@ def commit_month_dislocation(month_number):
             print("число не было обработано")
 
 
-commit_month_dislocation(3)
+commit_month_dislocation(4)
 print("successnyi success")
