3860cfbf7f9e4962d178f1fbea2ba2b4e0db42fc
Khakim <beksapin@mail.ru>
2021-11-14 16:10:09 +0600

    manual dislocation uploading 2021/11/06 21:00

--- src/algorithms/plan_fact_module/__init__.py
+++ src/algorithms/plan_fact_module/__init__.py
@@ -5,14 +5,16 @@ import psycopg2
 def commit_to_db(df):
     df = df[['ShippingDate', 'CarAmount', 'FromStationName', 'ToStationName', 'CargoEtsngName']]
     conn = psycopg2.connect(dbname='flow_map', user='postgres',
-                            password='root', host='localhost')#134.209.101.123
+                            password='root', host='134.209.101.123')#134.209.101.123
     cursor = conn.cursor()
+
+    df = df[df['ShippingDate'].notna()]
 
     cols = ", ".join([str(i) for i in df.keys()])
     for i, row in df.iterrows():
         try:
             sql = "INSERT INTO " + "PLAN_CARRIAGE" + " (" + cols + ") VALUES (" + "%s," * (len(row) - 1) + "%s)"
-            
+            #print(row)
             cursor.execute(sql, tuple(row))
 
             conn.commit()
@@ -23,23 +25,23 @@ def commit_to_db(df):
     conn.close()
 
 
-df = pd.read_excel("KBL.xlsx")
-df['CarAmount'] = df['CarAmount'].fillna(0)
+# df = pd.read_excel("KBL.xlsx")
+# df['CarAmount'] = df['CarAmount'].fillna(0)
 #commit_to_db(df)
 print(1)
-df = pd.read_excel("TAL.xlsx")
+df = pd.read_excel("TAL.xlsx", engine="openpyxl")
 df['CarAmount'] = df['CarAmount'].fillna(0)
-#commit_to_db(df)
+commit_to_db(df)
 print(1)
-df = pd.read_excel("BMZ.xlsx")
+df = pd.read_excel("BMZ.xlsx", engine="openpyxl")
 df['CarAmount'] = df['CarAmount'].fillna(0)
-#commit_to_db(df)
+commit_to_db(df)
 print(1)
-df = pd.read_excel("KAL.xlsx")
+df = pd.read_excel("KAL.xlsx", engine="openpyxl")
 df['CarAmount'] = df['CarAmount'].fillna(0)
-#commit_to_db(df)
+commit_to_db(df)
 print(1)
-df = pd.read_excel("NEV2.xlsx")
+df = pd.read_excel("NEV.xlsx", engine="openpyxl")
 df['CarAmount'] = df['CarAmount'].fillna(0)
 commit_to_db(df)
 print(1)
